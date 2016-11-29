f3fd5909e5f9f9cd017379100de79aae22039ec4
CDAGaming <videodude>
2016-11-29 09:17:51 -0600

    Website Initial

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function HomePage() {
+	window.open('index.html', '_self');
+}
+
+function NewsPage() {
+	window.open('News.html', '_self');
+}
+
+function HappyWheels() {
+	window.open('Games/HappyWheels.html', '_self');
+}
