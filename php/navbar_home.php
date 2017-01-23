f7d08b1bf73b468db61b90eacb6d1bfd843d447a
CDAGaming <cstack2011@yahoo.com>
2017-01-23 12:20:01 -0600

    PHP Prep

---
+++ php/navbar_home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">
+			<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">TeamCDA</a>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav">
+				<li class="active"><a href="#" onclick="HomePage()">Home</a></li>
+				<li class="dropdown"><a href="#" onclick="AboutPage()" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About<span class="caret"></span></a>
+				<ul class="dropdown-menu">
+				<li><a href="#" onclick="ContactPage()">Contact Us</a></li>
+				</ul>
+				</li>
+				<li><a href="#" onclick="NewsPage()">News</a></li>
+				<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Games<span class="caret"></span></a>
+				<ul class="dropdown-menu">
+				<li><a href="#" onclick="HappyWheels()">Happy Wheels</a></li>
+				</ul>
+				</li>
+				</ul>
+			</div>
+		</div>
+		</nav>
+</html>
