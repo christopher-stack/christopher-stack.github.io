e5e4d896dcada88a9a58a6fe144c89f4a3c18bda
CDAGaming <videodude12>
2017-04-13 12:10:25 -0500

    Reduce Timeout & Fix NonValid Cursor on Footer

--- js/script.js
+++ js/script.js
@@ -1,16 +1,9 @@
 var time;
 
 function calcTime() {
-	time = setTimeout(showPage, 3000);
+	time = setTimeout(showPage, 2000);
 }
 function showPage() {
 	document.getElementById("loader").style.display = "none";
 	document.getElementById("webDiv").style.display = "block";
-}
-function TwitterPG() {
-	window.open('https://twitter.com/CDA_Gamers', '_system');
-}
-
-function YoutubePG() {
-	window.open('https://t.co/gvolbJr5ng', '_system');
 }
