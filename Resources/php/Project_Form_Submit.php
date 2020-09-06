12e506c53ff1975a3a2c87e072c4f4dcc0b0057e
CDAGaming <cstack2011@yahoo.com>
2020-09-06 15:56:57 -0500

    [Fix] Form prettification
    Now displaying in a readable format

--- Resources/php/Project_Form_Submit.php
+++ Resources/php/Project_Form_Submit.php
@@ -8,18 +8,16 @@
 		<table>
 		<?php
 
-
 			foreach ($_POST as $key => $value) {
 				echo "<tr>";
 				echo "<td>";
-				echo $key;
-				echo "</td>";
+				echo str_replace('_', ' ', $key);
+				echo ":</td>";
 				echo "<td>";
 				echo $value;
 				echo "</td>";
 				echo "</tr>";
 			}
-
 
 		?>
 		</table>
