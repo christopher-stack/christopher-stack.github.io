f590d4c2ca1d5f30de0f89986a797cac4c50c1da
CDAGaming <cstack2011@yahoo.com>
2020-09-06 01:04:12 -0500

    [Change] Force rename p2

---
+++ Resources/static/js/script.js
@@ -0,0 +1,8 @@
+var time;
+function calcTime() {
+	time = setTimeout(showPage, 2000);
+}
+function showPage() {
+	document.getElementById("loader").style.display = "none";
+	document.getElementById("webDiv").style.display = "block";
+}
