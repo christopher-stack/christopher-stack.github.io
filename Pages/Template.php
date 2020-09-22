994af766235c0ce8c68cf79024ca069a6e6e063c
CDAGaming <cstack2011@yahoo.com>
2020-09-22 10:39:22 -0500

    [Change] Convert Template to php and fix navbar formatting

--- Pages/Template.php
+++ Pages/Template.php
@@ -1,3 +1,14 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] !== true){
+    header("location: Login.php");
+    exit;
+}
+?>
+
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
@@ -36,33 +47,43 @@
 			</button>
 		<a class="navbar-brand" href="#">TeamCDA</a>
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-		<ul class="navbar-nav mr-auto">
-			<li class="nav-item">
-			<a class="nav-link" href="../index.php"><i class="fas fa-home"></i> Home </a>
-			</li>
-			<li class="nav-item disabled">
-			<a class="nav-link" href="#"><i class="fas fa-exclamation-circle"></i> About </a>
-			</li>
-		<li class="nav-item dropdown">
-		<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"> Apps </a>
-			<div class="dropdown-menu" aria-labelledby="Preview">
-				<a class="dropdown-item" href="../Pages/App_APIHub.html"> APIHub - Pokemon GO </a>
-				<a class="dropdown-item" href="../Pages/NecroEase/index.html"> NecroEase - Pokemon GO </a>
-			</div>
-		</li>
-		<li class="nav-item dropdown">
-		<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="fas fa-gamepad"></i> Games </a>
-			<div class="dropdown-menu" aria-labelledby="Preview">
-				<a class="dropdown-item" href="../Pages/Game_Pacman.html">Bandai Namco - Pac-Man</a>
-			</div>
-		</li>
-		<li class="nav-item dropdown">
-		<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="fas fa-download"></i> Downloads </a>
-			<div class="dropdown-menu" aria-labelledby="Preview">
-				<a class="dropdown-item disabled" href="#"> All Downloads </a>
-			</div>
-		</li>
-		</ul>
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="../index.php"><i class="fas fa-home"></i> Home </a>
+				</li>
+				<li class="nav-item disabled">
+					<a class="nav-link" href="#"><i class="fas fa-exclamation-circle"></i> About </a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"> Apps </a>
+					<div class="dropdown-menu" aria-labelledby="Preview">
+						<a class="dropdown-item" href="./App_APIHub.html"> APIHub - Pokemon GO </a>
+						<a class="dropdown-item" href="./NecroEase/index.html"> NecroEase - Pokemon GO </a>
+					</div>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="fas fa-gamepad"></i> Games </a>
+					<div class="dropdown-menu" aria-labelledby="Preview">
+						<a class="dropdown-item" href="./Game_Pacman.html">Bandai Namco - Pac-Man</a>
+					</div>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="fas fa-download"></i> Downloads </a>
+					<div class="dropdown-menu" aria-labelledby="Preview">
+						<a class="dropdown-item disabled" href="#"> All Downloads </a>
+					</div>
+				</li>
+			</ul>
+			<ul class="navbar-nav">
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user"></i> <?php echo htmlspecialchars($_SESSION["username"]); ?></a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="Preview">
+						<a class="dropdown-item" href="../404.html">Profile</a>
+						<a class="dropdown-item" href="../Resources/php/Reset_Password.php">Reset password</a>
+						<a class="dropdown-item" href="../Resources/php/Logout.php">Sign out</a>
+					</div>
+				</li>
+			</ul>
 		</div>
 	</nav>
 	<!-- NAVIGATION HEADER END -->
