974241a76b9a36a3507455181f93cde2aa03df41
CDAGaming <cstack2011@yahoo.com>
2020-11-17 11:47:15 -0600

    [Change] Update Bootstrap to 5.0.0-alpha3; Updated cdn + elements

--- Resources/php/server/Reset_Password.php
+++ Resources/php/server/Reset_Password.php
@@ -78,11 +78,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
 	<!-- Style Scripts -->
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat|Roboto">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-CuOF+2SnTUfTwSZjCXf01h7uYhfOBuxIhGKPbfEJ3+FqH/s6cIFN9bGr1HmAg4fQ" crossorigin="anonymous">
 	<link rel="stylesheet" href="../../static/css/style.css" type="text/css">
 	<!-- End of Style Scripts -->
 	<!-- Include all compiled plugins (below), or include individual files as needed -->
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js" integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-popRpmFF9JQgExhfw5tZT4I9/CI5e2QcuUZPOVXb1m7qUmeR2b50u+YFEYe1wgzy" crossorigin="anonymous"></script>
 	<script src="https://kit.fontawesome.com/ccfb95e64e.js" crossorigin="anonymous"></script>
 	<script src="../../static/js/script.js"></script>
 	<!-- Favicon Code - Include ALL Below -->
