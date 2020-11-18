fe65ea45e718698393068107094b12597ec1fdbc
bayz <zhuoyu.yang@ndus.edu>
2020-11-18 16:59:22 -0600

    [Change] Add navbar to RegisterEmployer.php

--- Resources/php/server/RegisterEmployer.php
+++ Resources/php/server/RegisterEmployer.php
@@ -259,7 +259,89 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST)) {
 	<!-- END FAVICON CODE -->
 </head>
 <body>
-    <div class="register-wrapper">
+    <!-- NAVIGATION HEADER START -->
+	<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle Navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<a class="navbar-brand" href="#">TeamCDA</a>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="../index.php"><i class="fas fa-home"></i> Home </a>
+				</li>
+				<li class="nav-item disabled">
+					<a class="nav-link" href="#"><i class="fas fa-exclamation-circle"></i> About </a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"> Apps </a>
+					<div class="dropdown-menu" aria-labelledby="Preview">
+						<a class="dropdown-item" href="#"> APIHub - Pokemon GO </a>
+						<a class="dropdown-item" href="#"> NecroEase - Pokemon GO </a>
+					</div>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="fas fa-gamepad"></i> Games </a>
+					<div class="dropdown-menu" aria-labelledby="Preview">
+						<a class="dropdown-item" href="#">Bandai Namco - Pac-Man</a>
+					</div>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="fas fa-download"></i> Downloads </a>
+					<div class="dropdown-menu" aria-labelledby="Preview">
+						<a class="dropdown-item disabled" href="#"> All Downloads </a>
+					</div>
+				</li>
+			</ul>
+			<ul class="navbar-nav">
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user"></i> <?php echo htmlspecialchars($_SESSION["firstName"] . " " . $_SESSION["lastName"]); ?></a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="Preview">
+						<?php
+							// Add appropriate links based on role
+							if($_SESSION["role"] == "jobseeker"){
+								echo "<a";
+								echo " class=\"dropdown-item\"";
+								echo " href=\"../Pages/Project_Form.php\">Profile</a>";
+								echo "<a";
+								echo " class=\"dropdown-item\"";
+								echo " href=\"../Resources/static/error/404.html\">Application history</a>";
+								echo "<a";
+								echo " class=\"dropdown-item\"";
+								echo " href=\"../Resources/static/error/404.html\">Search jobs</a>";
+								echo "<a";
+								echo " class=\"dropdown-item\"";
+								echo " href=\"../Resources/static/error/404.html\">View jobs by category</a>";
+								echo "<a";
+								echo " class=\"dropdown-item\"";
+								echo " href=\"../Resources/static/error/404.html\">View jobs by company</a>";
+							} else if($_SESSION["role"] == "employer"){
+								echo "<a";
+								echo " class=\"dropdown-item\"";
+								echo " href=\"../Resources/static/error/404.html\">Company profile</a>";
+								echo "<a";
+								echo " class=\"dropdown-item\"";
+								echo " href=\"../Resources/static/error/404.html\">Post new position</a>";
+								echo "<a";
+								echo " class=\"dropdown-item\"";
+								echo " href=\"../Resources/static/error/404.html\">Edit positions</a>";
+								echo "<a";
+								echo " class=\"dropdown-item\"";
+								echo " href=\"../Resources/static/error/404.html\">Review applicants</a>";
+								echo "<a";
+								echo " class=\"dropdown-item\"";
+								echo " href=\"../Resources/static/error/404.html\">Search applicants</a>";
+							}
+						?>
+						<a class="dropdown-item" href="./Reset_Password.php">Reset password</a>
+						<a class="dropdown-item" href="./Logout.php">Sign out</a>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</nav>
+	<!-- NAVIGATION HEADER END -->
+    <div class="registerEmp-wrapper">
         <h2>Register HR Employee</h2>
         <p>Please fill out this form to create an employee account.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
